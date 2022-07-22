3e30dc6a1142f708ce3e5db42312ff4c5b2d0cce
anchor <anchorlightforge@gmail.com>
2022-07-22 01:37:59 -0400

    Shaders and blockout code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    CharacterController charControl;
+    Transform camDir;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camDir = Camera.main.transform;
+    }
+    [SerializeField] float gunRange = 200;
+    public void Fire()
+    {
+        RaycastHit gunCheck;
+        if(Physics.Raycast(transform.position,camDir.forward,out gunCheck,gunRange,enemyMask))
+        {
+            //deal damage if enemy found
+        }
+    }
+[SerializeField] LayerMask environmentMask,enemyMask;
+[SerializeField] float groundCheckRange = 1;
+    bool GroundCheck()
+    {
+        RaycastHit groundCheck;
+        if(Physics.Raycast(transform.position,-transform.up,out groundCheck,groundCheckRange))
+        {
+            return true;
+        }
+        return false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(GroundCheck())
+        {
+            
+        }
+        else 
+        {
+            charControl.Move(Vector3.down*(9.8f*9.8f));
+
+        }
         float moveHor = Input.GetAxisRaw("Horizontal");
         
         float moveVert = Input.GetAxisRaw("Vertical");
@@ -25,8 +56,9 @@ public class PlayerMovement : MonoBehaviour
         
         if(Input.GetButtonDown("Fire1"))
         {
-
+            Fire();
         }
+
 
         if(Input.GetButtonDown("Swap"))
         {
